1e1efe40f5582cf1e872cb1e9a2e9b64d4fc4467
Oskar Taubert <oskar.taubert@kit.edu>
2020-05-10 16:31:42 +0200

    updated script to check proteinnet msas

--- scripts/check_alignment.py
+++ scripts/check_alignment.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python
 
 import os
+import gzip
+
+import h5py
+from Bio import AlignIO
+from Bio import SeqIO
 
 from nnicotine import datasets
 
-partition = "training_30"
+test_ids = [
+    "2J58_1_A",
+    "2QXX_1_A",
+    "3EAF_1_A",
+    "3RZG_1_A",
+    "3VC5_d3vc5a1",
+    "4W8Q_1_A",
+    "4XHT_1_A",
+]
 
-infile = os.path.join(data_root_dir, "proteinnet/data/casp12/{}".format(partition))
 
-file_pointer = open(infile, 'r')
-next_entry = datasets.preprocess.read_entry(file_pointer)
+data_root_dir = os.environ['DATA_PATH']
 
-testids = []
+proteinnet_ds = h5py.File(os.path.join(data_root_dir, "proteinnet/data/preprocessed/casp12_30.hdf5"), 'r')
 
-while next_entry is not None:
-    ID = next_entry['id']
-    if os.path.isfile(os.path.join(data_root_dir, "proteinnet-raw/raw/"+ID+".a2m.gz")):
-        testids.append(ID)
-    if len(testids) >= 10:
-        break
-
-for ID in testids:
-    pass
-
+for ID in test_ids:
+    print(ID)
+    print(proteinnet_ds[ID]['primary'][()].tostring().decode('utf-8'))
+    alignment_file = os.path.join(data_root_dir, "proteinnet-raw/raw/{}.a2m.gz".format(ID))
+    with gzip.open(alignment_file, 'rt') as f:
+        l = None
+        alignment = SeqIO.parse(f, 'fasta')
+        for record in alignment:
+            if l is None:
+                print(record.id)
+                l = len(record)
+                print(l)
+            else:
+                if len(record) != l:
+                    print(record.id)
+                    print(len(record))
+                    break
