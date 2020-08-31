c37f6787b5363999c1c5d40753c62e4a71116f42
Oskar Taubert <oskar.taubert@kit.edu>
2020-08-31 16:45:57 +0200

    fixed typo and file name mistake

--- scripts/dataset_job.sh
+++ scripts/dataset_job.sh
@@ -2,13 +2,14 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=48
-#SBATCH --time=01:00:00
-#SBATCH --output==logs/cath_gen.out%j
-#SBATCH --error==logs/cath_gen.err%j
+#SBATCH --time=00:30:00
+#SBATCH --output=logs/cath_gen.out%j
+#SBATCH --error=logs/cath_gen.err%j
 #SBATCH --mail-user=oskar.taubert@kit.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 
+export PATH=$PATH:/home/kit/scc/qx6387/hh-suite/build/bin:/home/kit/scc/qx6387/hh-suite/build/scripts
 export KMP_AFFINITY=compact,1.0
 
-mpirun --bind-to core --map-by node rsync -r $DATA_PATH/uniref $LOCALSCRATCH
-mpirun --bind-to core --map-by node python generate_dataset.py
+mpirun --bind-to core --map-by node rsync -r $DATA_PATH/uniref $TMP
+mpirun --bind-to core --map-by node python generate_cath_dataset_msas.py
