b90f0cdfca2092764c7934eb7918e694d8f46f2c
Oskar Taubert <oskar.taubert@kit.edu>
2020-09-17 11:27:04 +0200

    running through casp derived datasets

--- scripts/explore_data.py
+++ scripts/explore_data.py
@@ -3,14 +3,31 @@
 import os
 
 from nnicotine import datasets
+from nnicotine import transforms
 
 data_root_dir = os.environ['DATA_PATH']
+root = os.path.join(data_root_dir, "nnicotine")
+cath_version = '20200708'
+mode = "toy"
 
-ds = datasets.ProteinNet(os.path.join(data_root_dir, 'proteinnet/data/'), mode=30, version=7, download=True, preprocess=True)
-
-print(len(ds))
+ds = datasets.CATHDerived(root, mode=mode, version=cath_version, generate=False)
 
 # TODO histogram sequence lengths and MSA effective number of sequences
 
+nsequences = []
+alengthts = []
+
+Tsample, Ttarget = transforms.get_transforms()
+
 for i, (x, y) in enumerate(ds):
-    print(i, x)
+    # print(i)
+    # print(x['sequence'], x['msa'][0].seq)
+    nsequences.append(len(x['msa']))
+    alengthts.append(x['msa'].get_alignment_length())
+
+    x = Tsample(x)
+
+
+    break
+
+# TODO plot
