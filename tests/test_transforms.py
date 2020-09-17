510a98b5271b61f962398016df1469a064cfdeec
Oskar Taubert <oskar.taubert@kit.edu>
2020-09-17 11:27:58 +0200

    added unit test sceletons for transforms

---
+++ tests/test_transforms.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+from pytest import approx
+
+
+def test_RandomDrop():
+    sample = None
+    raise
+
+def test_TrimToTarget():
+    raise
+
+def test_ComputeCouplings():
+    raise
+
+def test_ToCategorical():
+    raise
+
+def test_ToTensor_sample():
+    raise
+
+def test_ToTensor_label():
+    raise
+
+def test_ToDistance():
+    raise
+
+def test_ToBinnedDistance():
+    raise
