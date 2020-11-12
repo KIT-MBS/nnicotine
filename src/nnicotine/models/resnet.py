f7c688db4357b11ab53c9df3a2c8d4c2848240eb
Oskar Taubert <oskar.taubert@kit.edu>
2020-11-12 12:25:36 +0100

    implemented resnet as described in alphafold paper

--- src/nnicotine/models/resnet.py
+++ src/nnicotine/models/resnet.py
@@ -3,38 +3,38 @@ import torch.nn as nn
 from ..modules import PositionalEncoding, OuterConcatenation
 
 class Block(nn.Module):
-    def __init__(self, channels, kernel_size):
+    def __init__(self, channels, dilation, dropout=0.85):
         super(Block, self).__init__()
-        # TODO dilation
-        # TODO dropout
-        # TODO normalization
-        # TODO topology
 
         self.n = nn.BatchNorm2d(channels)
-        self.r1 = nn.ELU()
-        self.c1 = nn.Conv2d(channels, channels, kernel_size, padding=int(kernel_size/2))
-
-        return
+        self.r = nn.ELU()
+        self.c = nn.Conv2d(channels, channels, 3, dilation=dilation, padding=dilation)
+        self.drop = nn.Dropout(p=0.85)
 
     def forward(self, x):
         x = self.n(x)
 
-        return x + self.c1(self.r1(x))
+        return x + self.drop(self.c(self.r(x)))
 
 # TODO fields
 class ResNet(nn.Module):
-    def __init__(self, d=64, num_blocks=8, num_classes=64):
+    def __init__(self, num_classes=64):
         super(ResNet, self).__init__()
-        self.embed_dim = 30
+        embed_dim = 256
 
-        self.embedding = nn.Embedding(21, self.embed_dim)
+        self.embedding = nn.Embedding(21, embed_dim)
         self.positional = PositionalEncoding()
         self.outer_cat = OuterConcatenation()
-        blocks = [Block(d, 3) for i in range(num_blocks)]
+        dilations = [1, 2, 4, 8]
+
+        blocks = [Block(256, dilations[i%len(dilations)]) for i in range(7*4)]
+        blocks.extend([nn.BatchNorm2d(256), nn.ELU(), nn.Conv2d(256, 128, 1)])
+        # TODO should be 48*4 blocks
+        blocks.extend([Block(128, dilations[i%len(dilations)]) for i in range(12*4)])
+
         self.blocks = nn.Sequential(*blocks)
-        self.pre_blocks = nn.Sequential(nn.Conv2d(2*self.embed_dim + 2 + 20*20, d, kernel_size=1)) # TODO norm
-        self.post_blocks = nn.Sequential(nn.ELU(), nn.Conv2d(d, num_classes, kernel_size=1))
-        return
+        self.pre_blocks = nn.Sequential(nn.Conv2d(2*embed_dim + 2 + 20*20, 256, kernel_size=1))
+        self.post_blocks = nn.Sequential(nn.ELU(), nn.Conv2d(128, num_classes, kernel_size=1))
 
     def forward(self, x):
         seq = x['sequence']
