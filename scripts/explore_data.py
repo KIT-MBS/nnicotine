3681391d05d0a2d92e16431f5bc985cd6ff606fd
Oskar Taubert <oskar.taubert@kit.edu>
2020-11-19 15:47:49 +0100

    finished dataset exploration script for now

--- scripts/explore_data.py
+++ scripts/explore_data.py
@@ -1,28 +1,60 @@
 #!/usr/bin/env python
 
 import os
+from itertools import chain
+import numpy as np
+import matplotlib.pyplot as plt
+from mpi4py import MPI
 
 from nnicotine import datasets
-from nnicotine import transforms
+from nnicotine.utils import compute_msa_weights
 
 
 root = os.path.join(os.environ['DATA_PATH'], "nnicotine")
 cath_version = '20200708'
-mode = "toy"
+modes = ["toy"]
 
-ds = datasets.CATHDerived(root, mode=mode, version=cath_version, generate=False)
 
-nsequences = []
-neffectivesequences = []
-lengths = []
-gappedlengths = []
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
 
-Tsample, Ttarget = transforms.get_transforms()
+# NOTE preserve sample order
 
-for i, (x, y) in enumerate(ds):
-    lenghts.append(len(x['seq']))
-    gappedlengths.append(x['msa'].get_alignment_length())
-    nsequences.append(len(x['msa'][0]))
-    # TODO compute effective sequences
+for mode in modes:
+    ds = datasets.CATHDerived(root, mode=mode, version=cath_version, generate=False)
 
-# TODO histogram, plot and save
+    nsequences = []
+    neffectivesequences = []
+    lengths = []
+    gappedlengths = []
+
+    # rank_samples = [int(np.ceil(len(ds)/size))] + [int(len(ds)/size) for i in range(size-1)]
+    # offset = 0 if rank == 0 else sum(rank_samples[rank-1:rank])
+    limits = [i * int(len(ds)/size) for i in range(size)] + [len(ds)]
+
+    for i in range(limits[rank], limits[rank+1]):
+        x, y = ds[i]
+        lengths.append(len(x['sequence']))
+        gappedlengths.append(x['msa'].get_alignment_length())
+        nsequences.append(len(x['msa']))
+        neffectivesequences.append((compute_msa_weights(x['msa'])).sum())
+
+    lengths = comm.gather(lengths, root=0)
+    gappedlengths = comm.gather(gappedlengths, root=0)
+    nsequences = comm.gather(nsequences, root=0)
+    neffectivesequences = comm.gather(neffectivesequences, root=0)
+
+    if rank==0:
+        # NOTE flatten gathered data
+        lengths = np.array(list(chain.from_iterable(lengths)))
+        gappedlengths = np.array(list(chain.from_iterable(gappedlengths)))
+        nsequences = np.array(list(chain.from_iterable(nsequences)))
+        neffectivesequences = np.array(list(chain.from_iterable(neffectivesequences)))
+
+        assert len(lengths) == len(ds)
+        # NOTE ds stats save to file
+        np.save(f'data/{cath_version}_{mode}_lens.npy', lengths)
+        np.save(f'data/{cath_version}_{mode}_glens.npy', gappedlengths)
+        np.save(f'data/{cath_version}_{mode}_seq.npy', nsequences)
+        np.save(f'data/{cath_version}_{mode}_effseq.npy', neffectivesequences)
